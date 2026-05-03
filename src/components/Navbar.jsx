32c66b9061b3824d7a76e3a7e5f2a755bb6c7016
Faisal khan <faisalkhan.codes@gmail.com>
2026-05-03 12:01:05 +0500

    Added mandatory space between branding and Home link in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ export default function Navbar() {
         </motion.a>
 
         {/* Desktop Links */}
-        <div className="hidden lg:flex items-center gap-2 xl:gap-6">
+        <div className="hidden lg:flex items-center gap-2 xl:gap-6 ml-auto md:ml-20 xl:ml-32 justify-end">
           {navLinks.map((link, i) => (
             <motion.a key={link.href} href={link.href}
               initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 * i, duration: 0.3 }}
