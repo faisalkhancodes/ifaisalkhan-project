8f4449a83e28bad310a5145be7dea9be7874a43a
Faisal khan <faisalkhan.codes@gmail.com>
2026-05-03 12:05:23 +0500

    Maximized space between Navbar and Hero section for extreme clearance

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ function App() {
       <SocialSidebar />
       
       <main>
-        {/* Physical spacer for fixed Navbar clearance */}
-        <div className="h-16 md:h-20 w-full bg-[#050505]" />
+        {/* Physical spacer for fixed Navbar clearance - Maximized Space */}
+        <div className="h-48 md:h-72 w-full bg-[#050505]" />
         <Hero />
         <div className="w-full h-[150px]" />
         <About />
